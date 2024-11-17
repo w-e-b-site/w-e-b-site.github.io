fdd03a593c91365b76e6d2b679535b0500bd582f
w-e-b-site <2016420119@sdu.ac.kr>
2024-11-17 10:28:50 +0900

    commit

--- script/index.js
+++ script/index.js
@@ -1,9 +1,15 @@
-(function () {
+const listEl = document.querySelector(".list");
+// btnEl.addEventListener("click", () => {
+//     // trailerContainerEl.classList.remove("active");
+// });
+
+// (function () { })();
+window.onload = function () {
     fetch("/json/list.json")
         .then((response) => response.json())
         .then((json) => {
             // data = json.items;
             let data = JSON.parse(JSON.stringify(json.lists));
-            alert(JSON.stringify(data));
+            listEl.textContent = JSON.stringify(data);
         });
-})();
+};
