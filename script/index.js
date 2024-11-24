b32907a1c5a1a1986be691352fd8ec12dd05d313
w-e-b-site <2016420119@sdu.ac.kr>
2024-11-24 14:18:21 +0900

    commit

--- script/index.js
+++ script/index.js
@@ -1,16 +1,51 @@
-const listEl = document.querySelector(".list");
-// btnEl.addEventListener("click", () => {
-//     // trailerContainerEl.classList.remove("active");
-// });
+let readBookListTemp;
+const booklist = "/text/booklist.json";
+const bookListEl = document.querySelector(".bookList");
+const descriptEl = document.querySelector(".descript");
+
+// https://w-e-b-site.github.com/
+// XN--WR3B.XN--MK1BU44C
+
+function liOver(item) {
+    descriptEl.classList.add("showme1");
+    descriptEl.classList.remove("showme2");
+    descriptEl.innerHTML = readBookListTemp[item - 1].desc;
+}
+
+function liOut(item) {
+    descriptEl.classList.add("showme2");
+    descriptEl.classList.remove("showme1");
+}
+
+function liClick(item) {
+    location.href = `book.html?${readBookListTemp[item - 1].book}`;
+}
 
 // (function () { })();
 window.onload = function () {
-    fetch("/json/list.json")
+    let result = "";
+    fetch(booklist)
         .then((response) => response.json())
         .then((json) => {
-            // data = json.items;
-            let data = JSON.parse(JSON.stringify(json.lists));
-            listEl.textContent = JSON.stringify(data);
-            // listEl.innerHTML = "<div>";
+            const readBookList = json.booklist;
+            readBookListTemp = readBookList;
+            readBookList.forEach((item, i) => {
+                result +=
+                    "<li onclick=liClick(" +
+                    item.id +
+                    ") onmouseover=liOver(" +
+                    item.id +
+                    ") onmouseout=liOut(" +
+                    item.id +
+                    ")><span>" +
+                    item.id +
+                    "</span>" +
+                    item.book +
+                    "(" +
+                    item.hanja +
+                    ")";
+                ("</li>");
+            });
+            bookListEl.innerHTML = result;
         });
 };
